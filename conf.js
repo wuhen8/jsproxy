2b9c3134dc5d622485ceab49f95a83e3c24082c3
wuhen8 <93480904+wuhen8@users.noreply.github.com>
2022-02-10 16:48:14 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -89,7 +89,7 @@ jsproxy_config({
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
     'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
+      redir: 'https://cn.pornhub.com/'
     },
     'http://haha.com/': {
       content: 'Hello World'
